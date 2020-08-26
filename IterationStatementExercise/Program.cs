f297357541f2e108482f70356574bf7783e86be7
mgrupa89 <69479947+mgrupa89@users.noreply.github.com>
2020-08-26 11:23:56 -0500

    First commit to the Iteration Statement Repository

---
+++ IterationStatementExercise/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IterationStatementExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
